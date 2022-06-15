33195958acec063c706b0f20b7661226da1b3da5
Daniel Roe <daniel@roe.dev>
2022-06-15 10:51:48 +0100

    feat(nuxt): add warning in dev mode if layouts/pages do not have a single root node

--- packages/nuxt/src/pages/runtime/page.ts
+++ packages/nuxt/src/pages/runtime/page.ts
@@ -30,17 +30,31 @@ export default defineComponent({
 
     return () => {
       return h(RouterView, { name: props.name, route: props.route, ...attrs }, {
-        default: (routeProps: RouterViewSlotProps) => routeProps.Component &&
-            _wrapIf(Transition, routeProps.route.meta.pageTransition ?? defaultPageTransition,
-              wrapInKeepAlive(routeProps.route.meta.keepalive,
-                isNested && nuxtApp.isHydrating
-                  // Include route children in parent suspense
-                  ? h(routeProps.Component, { key: generateRouteKey(props.pageKey, routeProps) } as {})
-                  : h(Suspense, {
-                    onPending: () => nuxtApp.callHook('page:start', routeProps.Component),
-                    onResolve: () => nuxtApp.callHook('page:finish', routeProps.Component)
-                  }, { default: () => h(routeProps.Component, { key: generateRouteKey(props.pageKey, routeProps) } as {}) })
-              )).default()
+        default: (routeProps: RouterViewSlotProps) => {
+          if (!routeProps.Component) { return false }
+
+          const pageComponent = h(routeProps.Component, { key: generateRouteKey(props.pageKey, routeProps) } as {})
+          const transitionProps = routeProps.route.meta.pageTransition ?? defaultPageTransition
+
+          if (process.dev && process.client && transitionProps && pageComponent) {
+            setTimeout(() => {
+              if (pageComponent.el?.nodeName === '#comment') {
+                console.error(`\`${pageComponent.type.__file}\` does not have a single root node and will cause errors when navigating between routes.`)
+              }
+            })
+          }
+
+          return _wrapIf(Transition, transitionProps,
+            wrapInKeepAlive(routeProps.route.meta.keepalive,
+              isNested && nuxtApp.isHydrating
+                // Include route children in parent suspense
+                ? pageComponent
+                : h(Suspense, {
+                  onPending: () => nuxtApp.callHook('page:start', routeProps.Component),
+                  onResolve: () => nuxtApp.callHook('page:finish', routeProps.Component)
+                }, { default: () => pageComponent })
+            )).default()
+        }
       })
     }
   }
